7352dd2ab74d35769d244c147e0cdbd0a92a2dcd
Riaz Khan <khanriazaoif@gmail.com>
2018-03-28 13:51:26 -0500

    Added the 5 random quotes and the for loop

--- random-quote-v2 (1)/js/script.js
+++ random-quote-v2 (1)/js/script.js
@@ -2,3 +2,36 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+
+var quotes;
+var quote;
+var source;
+
+
+var quotes = [
+    {
+        quote: '“The Way Get Started Is To Quit Talking And Begin Doing.”';
+        source: 'Walt Disney';
+    },
+    {
+        quote: '“The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.”';
+        source: 'Winston Churchill';
+    },
+    {
+        quote: '“Don’t Let Yesterday Take Up Too Much Of Today.”';
+        source: 'Will Rogers';
+    },
+    {
+        quote: '“You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character.”';
+        source: 'Unknown';
+    },
+    {
+        quote: '“It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.”';
+        source: 'Vince Lombardi';
+    },
+];
+
+for (var i = 0; i < quotes.length; i++) {
+    quote = quotes[i].quote;
+    source = quotes[i].source;
+}
