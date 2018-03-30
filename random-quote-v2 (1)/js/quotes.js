0bfa98a7eb4dc56a7548bc177e5901e1c5e4a8a0
Riaz Khan <khanriazaoif@gmail.com>
2018-03-29 20:53:42 -0500

    Trying to add the getRandomQuote function.

---
+++ random-quote-v2 (1)/js/quotes.js
@@ -0,0 +1,22 @@
+quotes = [
+    {
+        quote: '“The Way Get Started Is To Quit Talking And Begin Doing.”',
+        source: 'Walt Disney'
+    },
+    {
+        quote: '“The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.”',
+        source: 'Winston Churchill'
+    },
+    {
+        quote: '“Don’t Let Yesterday Take Up Too Much Of Today.”',
+        source: 'Will Rogers'
+    },
+    {
+        quote: '“You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character.”',
+        source: 'Unknown'
+    },
+    {
+        quote: '“It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.”',
+        source: 'Vince Lombardi'
+    },
+];
--- random-quote-v2 (1)/js/script.js
+++ random-quote-v2 (1)/js/script.js
@@ -1,42 +1,69 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
 var quotes;
 var quote;
 var source;
 
-function getRandomQuote() {
-    var quote = Math.floor(Math.random() * quotes) + 1;
+function getRandomQuote(quotes){
+    for (var i = 0; i < quotes.length; i++) {
+       // quote = quotes[i].quote;
+        //source = quotes[i].source;
+        console.log(quotes);
+        }
+}
+
+getRandomQuote(quotes);
+
+function print(quotes) {
+    var outputDiv = document.getElementById('quote-box');
+    outputDiv.innerHTML = quotes;
+    console.log(quotes);
+}
+
+//print(quotes);
+
+
+
+for(var key in quotes){
+   console.log(key,' :', quotes[key]);
 }
 
 
 
-var quotes = [
-    {
-        quote: '“The Way Get Started Is To Quit Talking And Begin Doing.”';
-        source: 'Walt Disney';
-    },
-    {
-        quote: '“The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.”';
-        source: 'Winston Churchill';
-    },
-    {
-        quote: '“Don’t Let Yesterday Take Up Too Much Of Today.”';
-        source: 'Will Rogers';
-    },
-    {
-        quote: '“You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character.”';
-        source: 'Unknown';
-    },
-    {
-        quote: '“It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.”';
-        source: 'Vince Lombardi';
-    },
-];
+/*
+function getRandomQuote() {
+    var quote = Math.floor(Math.random() * quotes.length) + 1;
+    return quote;
+}
+console.log(getRandomQuote() );
+getRandomQuote();
+*/
+
+/*
+function print(quotes) {
+    var outputDiv = document.getElementById('quote-box"');
+    outputDiv.innerHTML = quote;
+  
 
 for (var i = 0; i < quotes.length; i++) {
     quote = quotes[i].quote;
     source = quotes[i].source;
+    }
+}
+print(quotes);
+*/
+
+
+
+  
+
+var student = {
+    name: 'Riaz',
+    grades: [95, 96, 99, 100]
+};
+for (var key in student) {
+    console.log(key);
 }
