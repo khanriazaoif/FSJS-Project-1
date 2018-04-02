b7c2eb419bd4c822c60215ece4b3ad6103874df1
Riaz Khan <khanriazaoif@gmail.com>
2018-04-02 14:35:38 -0500

    Added '' to the undefined quotes.
    I added '' to source in order to make the response blank. this is however causing a dash to appear in the html. I am going to work on that on the next revision.

--- random-quote-v2 (1)/js/quotes.js
+++ random-quote-v2 (1)/js/quotes.js
@@ -7,7 +7,7 @@ var quotes = [
     },
     {
         quote: '“Here\'s to the crazy ones. The misfits. The rebels. The troublemakers. The round pegs in the square holes. The ones who see things differently. They\'re not fond of rules. And they have no respect for the status quo. You can quote them, disagree with them, glorify or vilify them. About the only thing you can\'t do is ignore them. Because they change things. They push the human race forward. While some see them as the crazy ones, we see genius. Because the people who are crazy enough to think they can change the world, are the ones who do.”',
-        source: '',
+        source: ' ',
         citation: 'Apple TV Ad',
         year: 1998
     },
@@ -25,7 +25,7 @@ var quotes = [
     },
     {
         quote: '“Here’s to those who have always seen things differently. The ones who follow a vision, not a path. Where others percieve first as valuable, you value the first thing that actually matters. While others are distracted by the new, you focus on the significance of a whole new take. Even before you could see how, you never doubted we would change things. And then we did. Together.”',
-        source: '',
+        source: ' ',
         citation: 'Apple Keynote Address',
         year: 2014
     },
