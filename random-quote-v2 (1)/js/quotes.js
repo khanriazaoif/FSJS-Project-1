1e2a78a47493499993566e0664571a4abe2482f5
Riaz Khan <khanriazaoif@gmail.com>
2018-04-03 15:11:26 -0500

    Updated quotes

--- random-quote-v2 (1)/js/quotes.js
+++ random-quote-v2 (1)/js/quotes.js
@@ -5,14 +5,14 @@ var quotes = [
         citation: "Steve Jobs’s 2005 commencement address at Stanford University.",
         year: 2005
     },
-    */
+    
     {
         quote: '“Here\'s to the crazy ones. The misfits. The rebels. The troublemakers. The round pegs in the square holes. The ones who see things differently.”',
-        source: '',
+        source: 'Apple.inc',
         citation: 'Apple TV Ad',
         year: 1998
     },
-    /*
+    
     {
         quote: '“It Just Works.”',
         source: 'Apple.inc',
@@ -25,11 +25,18 @@ var quotes = [
         citation: 'Apple Shareholders Meeting',
         year: 2006
     },
-    */
+     */
     {
         quote: '“Here’s to those who have always seen things differently. The ones who follow a vision, not a path. Where others percieve first as valuable, you value the first thing that actually matters.”',
-        source: ' ',
+        source: 'Apple.inc',
         citation: 'Apple Keynote Address',
         year: 2014
+    },
+   
+    {
+        quote: '“This is your last chance. After this, there is no turning back. You take the blue pill - the story ends, you wake up in your bed and believe whatever you want to believe. You take the red pill - you stay in Wonderland and I show you how deep the rabbit-hole goes.”',
+        source: 'Morpheus',
+        citation: '',
+        year: '2014'
     },
 ];
