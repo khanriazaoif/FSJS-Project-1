4f0bee157ce58c2a83337af4c962129df235ea3c
Riaz Khan <khanriazaoif@gmail.com>
2018-04-08 11:37:11 -0500

    I have added partial comments and have added tags in the quotes array. Adding if statement tags to the js page.

--- random-quote-v2 (1)/js/quotes.js
+++ random-quote-v2 (1)/js/quotes.js
@@ -1,40 +1,47 @@
+//an array of Javascript objects holding the data for my quotes. The array is called quotes and is available in the global scope.
+
 var quotes = [
     {
         quote: '“Have the courage to follow your heart and intuition."',
         source: 'Steve Jobs',
         citation: "Steve Jobs’s 2005 commencement address at Stanford University.",
-        year: 2005
+        year: 2005,
+        tags: 'Speech'
     },
     
     {
         quote: '“Here\'s to the crazy ones. The misfits. The rebels. The troublemakers.”',
         source: 'Apple.inc',
         citation: 'Apple TV Ad',
-        year: ''
+        tags: 'Commercial'
     },
     
     {
         quote: '“It Just Works.”',
         source: 'Apple.inc',
         citation: 'WWDC',
-        year: 2011
+        year: 2011,
+        tags: 'Commercial'
     },
     {
         quote: '“Everyone wants a MacBook Pro because they are so bitchin.”',
         source: 'Steve Jobs',
         citation: 'Apple Shareholders Meeting',
-        year: 2006
+        year: 2006,
+        tags: 'Speech'
     },
     
     {
         quote: '“Here’s to those who have always seen things differently. The ones who follow a vision, not a path.”',
         source: 'Apple.inc',
         citation: 'Apple Keynote Address',
-        year: 2014
+        year: 2014,
+        tags: 'Speech'
     },
    
     {
-        quote: '“Be yourself; everyone else is already taken.”',
-        source: 'Oscar Wilde',
+        quote: '“You only live once, but if you do it right, once is enough.”',
+        source: 'Mae West',
+        tags: 'humor'
     },
 ];
--- random-quote-v2 (1)/js/script.js
+++ random-quote-v2 (1)/js/script.js
@@ -1,7 +1,7 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 
-
+// declaring variables in the global scope
 
 var quote
 var randomIndex;
@@ -9,11 +9,15 @@ var outputDiv;
 let string;
 var nIntervId;
 var color;
- 
+
+// function changeColor
+
     function changeColor() {
       nIntervId = setInterval(flashBackground, 1000);
     }
 
+// this function getRandomColor creats two variables. var color holds the symbol # and var letters holds a 16 character string that together combine into a hex color. The for loop takes the letters and uses math.floor and math.random * 16 to generate a different string each iteration of the loop. The variable color then returns the new random hex color.
+    
     function getRandomColor() {
         var letters = '0123456789ABCDEF';
         var color = '#';
@@ -22,20 +26,20 @@ var color;
         }
         return color;
       }
+// this function flashBackground has a variable oElem that selects the ID 'my_box' and changes the background of the color of the document to the color that was created in the getRandomColor function.     
 
     function flashBackground() {
         var oElem = document.getElementById('my_box').style.backgroundColor == getRandomColor();
       }
    
+// this function getRandomQuote creates a variabled called randomIndex that generates a number between 1 and the number of quotes that I have created. The function then returns the quote inside the quotes array using the randomIndex number that came from the math.floor and math.random.
 
 function getRandomQuote() {
     var randomIndex = Math.floor(Math.random() * quotes.length);
     return quotes[randomIndex];
 }
 
-printQuote();
-getRandomColor();
-flashBackground();
+//this function printQuote has a variable that contains the quote from the getRandomQuote function. It then outputs a string with quote array quote property plus the citation property. These are the two required strings that will always be present.
 
 function printQuote() {
     quote = getRandomQuote();
@@ -56,5 +60,11 @@ function printQuote() {
     outputDiv.innerHTML = string;
         
 }
+
+printQuote();
+getRandomColor();
+flashBackground();
+
+
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
