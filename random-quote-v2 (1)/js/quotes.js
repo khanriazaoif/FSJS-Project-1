dd0d46e5d9257682b966d748931a91ac10764354
Riaz Khan <khanriazaoif@gmail.com>
2018-04-08 13:04:26 -0500

    Updated the quotes array to remove quotes.
    Updated the quotes array to remove the quotes I had inserted as the CSS already added to the HTML so I was duplicating quotes.

--- random-quote-v2 (1)/js/quotes.js
+++ random-quote-v2 (1)/js/quotes.js
@@ -2,7 +2,7 @@
 
 var quotes = [
     {
-        quote: '“Have the courage to follow your heart and intuition."',
+        quote: 'Have the courage to follow your heart and intuition.',
         source: 'Steve Jobs',
         citation: "Steve Jobs’s 2005 commencement address at Stanford University.",
         year: 2005,
@@ -10,21 +10,21 @@ var quotes = [
     },
     
     {
-        quote: '“Here\'s to the crazy ones. The misfits. The rebels. The troublemakers.”',
+        quote: 'Here\'s to the crazy ones. The misfits. The rebels. The troublemakers.',
         source: 'Apple.inc',
         citation: 'Apple TV Ad',
         tags: 'Commercial'
     },
     
     {
-        quote: '“It Just Works.”',
+        quote: 'It Just Works.',
         source: 'Apple.inc',
         citation: 'WWDC',
         year: 2011,
         tags: 'Commercial'
     },
     {
-        quote: '“Everyone wants a MacBook Pro because they are so bitchin.”',
+        quote: 'Everyone wants a MacBook Pro because they are so bitchin.',
         source: 'Steve Jobs',
         citation: 'Apple Shareholders Meeting',
         year: 2006,
@@ -32,7 +32,7 @@ var quotes = [
     },
     
     {
-        quote: '“Here’s to those who have always seen things differently. The ones who follow a vision, not a path.”',
+        quote: 'Here’s to those who have always seen things differently. The ones who follow a vision, not a path.',
         source: 'Apple.inc',
         citation: 'Apple Keynote Address',
         year: 2014,
@@ -40,7 +40,7 @@ var quotes = [
     },
    
     {
-        quote: '“You only live once, but if you do it right, once is enough.”',
+        quote: 'You only live once, but if you do it right, once is enough.',
         source: 'Mae West',
         tags: 'humor'
     },
