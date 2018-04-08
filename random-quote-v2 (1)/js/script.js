b512ad0119088985cfa3539405ae9ee8013bcf25
Riaz Khan <khanriazaoif@gmail.com>
2018-04-08 14:43:35 -0500

    MInor changes. Background color changes and quotes change with click
    MInor changes. Background color changes and quotes change with click. Have one more task, to change the windows.setInterval

--- random-quote-v2 (1)/js/script.js
+++ random-quote-v2 (1)/js/script.js
@@ -9,7 +9,7 @@ var nIntervId;
 var color;
 // function changeColor
 function changeColor() {
-	nIntervId = setInterval(flashBackground, 1000);
+	nIntervId = setInterval(flashBackground, 3000);
 }
 // this function getRandomColor creats two variables. var color holds the symbol # and var letters holds a 16 character string that together combine into a hex color. The for loop takes the letters and uses math.floor and math.random * 16 to generate a different string each iteration of the loop. The variable color then returns the new random hex color.
 function getRandomColor() {
@@ -20,7 +20,7 @@ function getRandomColor() {
 	}
 	return color;
 }
-// this function flashBackground has a variable oElem that selects the ID 'my_box' and changes the background of the color of the document to the color that was created in the getRandomColor function.     
+// this function flashBackground has a variable oElem that selects the body and changes the background of the color of the document to the color that was created in the getRandomColor function.     
 function flashBackground() {
     var oElem = document.body.style.backgroundColor = getRandomColor();
 }
