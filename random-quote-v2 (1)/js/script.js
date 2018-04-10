6191cb125e1ef95c047079d171cc34f5d5b727e2
Riaz Khan <khanriazaoif@gmail.com>
2018-04-09 19:12:29 -0500

    Latest build. Trying to figure out how to rotate text on a set interval.

--- random-quote-v2 (1)/js/script.js
+++ random-quote-v2 (1)/js/script.js
@@ -3,23 +3,13 @@
 // declaring variables in the global scope
 var quote;
 var colorChange;
-var stopColor;
 var randomIndex;
 var outputDiv;
 let string;
-var changeBackgroundColor;
 var color;
-var changeQuoteInterval;
-//
-function stopTextColor() {
-    clearInterval(changeBackgroundColor, 1000);
-  } 
-//
-function changeQuote() {
-    changeQuoteInterval = setInterval(getRandomQuote,6000);
-}
+var flashTheQuote;
+var changeQuoteInterval;  
 // this function getRandomColor creats two variables. var color holds the symbol # and var letters holds a 16 character string that together combine into a hex color. The for loop takes the letters and uses math.floor and math.random * 16 to generate a different string each iteration of the loop. The variable color then returns the new random hex color.
-
 function getRandomColor() {
 	var letters = '0123456789ABCDEF';
 	var color = '#';
@@ -28,33 +18,29 @@ function getRandomColor() {
 	}
 	return color;
 }
-
-// this function flashBackground has a variable oElem that selects the body and changes the background of the color of the document to the color that was created in the getRandomColor function.     
-/*
-function flashBackground() {
-    var fb = document.body.style.backgroundColor = getRandomColor();
-}
-*/
-//
-// function changeColor
+// function changeColor creates a local variable called cc that takes the document body and changes the background to the hex color that came from the getRandomColor(); function.
 function changeColor() {
 	var cc = document.body.style.backgroundColor = getRandomColor();
 }
-
 // this function getRandomQuote creates a variabled called randomIndex that generates a number between 1 and the number of quotes that I have created. The function then returns the quote inside the quotes array using the randomIndex number that came from the math.floor and math.random.
 function getRandomQuote() {
 	var randomIndex = Math.floor(Math.random() * quotes.length);
 	return quotes[randomIndex];
 }
+// this function flashQuote selects the ID 'quoote-box' from index.html page and changes the innerHTML contents to getRandomQuote()
+// function flashQuote() {
+//     var fq = document.getElementById('quote-box').innerHTML = getRandomQuote();
+// }
 //
-function flashQuote() {
-    var fq = document.getElementById('quote-box') = getRandomQuote();
+function changeQuote() {
+    var quoteChange = setInterval(getRandomQuote,1000);
 }
 //this function printQuote has a variable that contains the quote from the getRandomQuote function. It then outputs a string with quote array quote property plus the citation property. These are the two required strings that will always be present. From there I am checking if there is a citation, year or tags and if there are then to display them. At the end the variable outputDiv gets the ID quote-box and changes the HTML to the string that is in the variable string.
 function printQuote() {
     quote = getRandomQuote();
-    colorChange = changeColor();
-  //  stopColor = stopTextColor();
+	colorChange = changeColor();
+	flashTheQuote = changeQuote();
+	//changeQuoteInterval = flashQuote();
     string = '<p class="quote">' + quote.quote + '</p>' + '<p class="source">' + quote.source;
 	if (quote.citation) {
 		string += '<span class="citation">' + quote.citation + '</span>'
@@ -67,17 +53,8 @@ function printQuote() {
 	}'</p>';
 	outputDiv = document.getElementById('quote-box');
 	outputDiv.innerHTML = string;
+	//outputDiv += string.setInterval(getRandomQuote, 1000);
 }
-//
-stopColor = stopTextColor();
-// These are the 3 different functions being called.
-//printQuote();
-//getRandomColor();
-//flashBackground();
-//
-function start(){
-    
-    changeQuote();
-}
+
 // this looks ofr  a click event and then runs the function printQuote.
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
