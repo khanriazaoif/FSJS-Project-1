c44b5e1e4a3d1a4564cb7750b0db03f6bbc341a2
Riaz Khan <khanriazaoif@gmail.com>
2018-05-29 21:03:17 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # project-1
+
+Project Instructions
+steps
+Create an array of JavaScript objects to hold the data for your quotes.
+
+-Name the array quotes.
+-The quotes array should be accessible in the global scope, meaning it shouldn't be inside of a function.
+
+Each quote object in the quotes array should have the following properties:
+
+-A quote property which contains a string: the text of the quote that will be displayed on the page.
+-A source property which contains a string identifying the creator of the quote. For example: "Mark Twain" or "Traditional Irish proverb.”
+-An optional citation property which contains a string identifying where the quote comes from, like a speech or publication. For example, "Famous Anonymous Jokes." If there is no known publication, do not include this property on the object.
+-An optional year property which contains a number identifying the year of the quote. For example, 1997. If there is no known year, then do not include this property on the object.
+
+Create a function named getRandomQuote which:
+
+-selects a random quote object from the quotes array.
+-returns the randomly selected quote object.
+
+Create a function named printQuote which follows these rules:
+
+-printQuote calls the getRandomQuote function and stores the returned quote object in a variable.
+-printQuote constructs a string containing the different properties of the quote object using the following HTML template:
+
+<p class="quote"> [quote here] </p>
+<p class="source"> [source here]
+  <span class="citation"> [citation here] </span>
+  <span class="year"> [year here] </span>
+</p>
+
+-printQuote only adds a <span class="citation"> if the quote object has a citation property.
+-printQuote only adds a <span class="year"> if the quote object has a year property.
+-printQuote displays the final HTML string to the page. You can use this JS snippet to accomplish that:
+
+document.getElementById('quote-box').innerHTML
---
+++ js/script.js
@@ -0,0 +1,46 @@
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+
+//I AM ATTEMPTING EXCEEDS EXPECTATIONS. PLEASE GRADE BASED ON THAT ASSUMPTION
+
+//function getRandomColor creats two variables. var color holds the symbol # and var letters holds a 6 character string that together combine into a hex color. (For example console.log(color) = #4473C0) The for loop takes the letters and uses math.floor and math.random * 16 to generate a different string each iteration of the loop. The variable color then returns the new random hex color.
+function getRandomColor() {
+	let letters = '0123456789ABCDEF';
+	let color = '#';
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;	
+}
+// function getRandomQuote creates a variabled called i that generates a number between 1 and the number of quotes that I have created. The function then returns the quote inside the quotes array using the i number that came from the math.floor and math.random. For example (console.log(quotes[i]) produces one of the random quotes)
+function getRandomQuote() {
+	let i = Math.floor(Math.random() * quotes.length);
+	return quotes[i];
+}
+// variable changeQuoteInterval is set equal to the setInterval function with a set interval of 30 seconds and the argument printQuote function is selected. The function changeHandler clears the interval. Then the printQuote function is run and this sets a new interval. 
+let changeQuoteInterval = setInterval(printQuote, 30000);
+function changeHandler() {
+    clearInterval(changeQuoteInterval);
+    printQuote();
+    changeQuoteInterval = setInterval(printQuote, 30000);
+}
+// when function printQuote is run it first changes the background color of the document (webpage) to the returned color from the getRandomColor function. Then var quote is set to the random quote from getRandomQuote. Then var string is created with the quote and source from the quotes array. Then an if statement checks if there is a citation, year or tags and only shows if there are any. At the end the outputDiv changes the innerHTML (the text with the HTML elements) to the output of the string variable from line 31
+function printQuote() {
+	document.body.style.backgroundColor = getRandomColor();
+	var quote = getRandomQuote();
+	var string = '<p class="quote">' + quote.quote + '</p>' + '<p class="source">' + quote.source;
+	if (quote.citation) {
+		string += '<span class="citation">' + quote.citation + '</span>'
+	}
+	if (quote.year) {
+		string += '<span class="year">' + quote.year + '</span>'
+	}
+	if (quote.tags) {
+		string += '<span class ="tags">' + ' ' + quote.tags + '</span>'
+	}
+	'</p>';
+	var outputDiv = document.getElementById('quote-box');
+	outputDiv.innerHTML = string;
+}
+// this looks for  a click event and then runs the function printQuote.
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
