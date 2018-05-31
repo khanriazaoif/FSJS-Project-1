5eccfd0c546be5bab94aae0d8041f8805a6ca248
Riaz Khan <khanriazaoif@gmail.com>
2018-05-30 19:11:27 -0500

    moved changeQuoteInterval into the printQuote function

--- js/script.js
+++ js/script.js
@@ -18,14 +18,15 @@ function getRandomQuote() {
 	return quotes[i];
 }
 // variable changeQuoteInterval is set equal to the setInterval function with a set interval of 30 seconds and the argument printQuote function is selected. The function changeHandler clears the interval. Then the printQuote function is run and this sets a new interval. 
-let changeQuoteInterval = setInterval(printQuote, 30000);
+
+// when function printQuote is run it first changes the background color of the document (webpage) to the returned color from the getRandomColor function. Then var quote is set to the random quote from getRandomQuote. Then var string is created with the quote and source from the quotes array. Then an if statement checks if there is a citation, year or tags and only shows if there are any. At the end the outputDiv changes the innerHTML (the text with the HTML elements) to the output of the string variable from line 31
+function printQuote() {
+	let changeQuoteInterval = setInterval(printQuote, 30000);
 function changeHandler() {
     clearInterval(changeQuoteInterval);
     printQuote();
     changeQuoteInterval = setInterval(printQuote, 30000);
 }
-// when function printQuote is run it first changes the background color of the document (webpage) to the returned color from the getRandomColor function. Then var quote is set to the random quote from getRandomQuote. Then var string is created with the quote and source from the quotes array. Then an if statement checks if there is a citation, year or tags and only shows if there are any. At the end the outputDiv changes the innerHTML (the text with the HTML elements) to the output of the string variable from line 31
-function printQuote() {
 	document.body.style.backgroundColor = getRandomColor();
 	var quote = getRandomQuote();
 	var string = '<p class="quote">' + quote.quote + '</p>' + '<p class="source">' + quote.source;
@@ -44,3 +45,5 @@ function printQuote() {
 }
 // this looks for  a click event and then runs the function printQuote.
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+printQuote();
